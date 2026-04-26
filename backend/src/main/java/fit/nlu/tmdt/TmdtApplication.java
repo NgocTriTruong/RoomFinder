cffba8d7b862c194617d053a104d38693e37b086
nTruong257 <22130306@st.hcmuaf.edu.vn>
2026-04-26 22:31:45 +0700

    fix TimeZone
    Set mặc định thành Asian/Ho_Chi_Minh

--- backend/src/main/java/fit/nlu/tmdt/TmdtApplication.java
+++ backend/src/main/java/fit/nlu/tmdt/TmdtApplication.java
@@ -20,6 +20,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class TmdtApplication {
 
     public static void main(String[] args) {
+        java.util.TimeZone.setDefault(java.util.TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
         SpringApplication.run(TmdtApplication.class, args);
     }
 }
